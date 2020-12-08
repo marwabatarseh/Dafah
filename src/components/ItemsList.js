3ef76e2d8ef9d0a76d315889e9528a04234b446f
abeer-rawash <abeer.rawa96@gmail.com>
2020-12-08 17:21:18 +0200

    image working and search functionalities

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -3,7 +3,7 @@ import { Link ,withRouter } from "react-router-dom" ;
 import axios from "axios";
 import Footer from './Footer';
 
-//hi
+
 const ClothesItem = props => (
     <tr>
         <td>{props.item.itemName}</td>
@@ -11,7 +11,6 @@ const ClothesItem = props => (
         <td>{props.item.type}</td>
         <td>{props.item.description}</td>
         <td>{props.item.phoneNumber}</td>
-        
         <td>
         <img src= {props.item.image} width="200" height="200" class="w3-round" alt="Norway"/>
         </td>
@@ -25,32 +24,28 @@ const ClothesItem = props => (
         </td>
     </tr>
 )
-
 class ItemsList extends Component {
-
     constructor(props) {
         super(props);
-
         this.deleteItem = this.deleteItem.bind(this);
-
         this.state = {
             items: [],
             filteredItems :[],
             SearchString:'',
+            category:'',
+            type: ''
         }
     }
-
     componentDidMount() {
          axios.get("http://localhost:3000/addItems/")   
             .then( res => {
                 this.setState({items: res.data})
-                console.log(this.state.items,"rendernyyy")
+                console.log(this.state.items,"hayniiii")
             })
             .catch((error) => {
                 console.log(error);
             })
     }
-
     deleteItem(id) {
         axios.delete("http://localhost:3000/addItems/" + id)
             .then(res => console.log(res.data));
@@ -58,34 +53,75 @@ class ItemsList extends Component {
             items: this.state.items.filter(el => el._id !== id)
         })
     }
-
     itemsList() {
         let listedItems = (this.state.filteredItems.length > 0)? this.state.filteredItems : this.state.items; 
-
         return listedItems.map(currentItem => {
             return <ClothesItem item = { currentItem } deleteItem = { this.deleteItem } key = { currentItem._id }/>; 
         })
     } 
+  
 
-    onSearch = e => {
-        let { items } = this.state
+
+    onChangecategory(e){
+        let { items } = this.state 
         let string = e.target.value
-        if(string.length > 0){
-           let filteredItems = items.filter(item => item.itemName.includes(string))
-           this.setState({SearchString:string,filteredItems:filteredItems})
-        }
-        else this.setState({SearchString:string,filteredItems:[]})
+        this.setState({
+            category :e.target.value 
+        },
+        )
+        let filteredItems = items.filter(item => item.category.includes(string))
+        this.setState({filteredItems:filteredItems})
     }
 
 
+    onChangetype(e){
+        let { filteredItems } = this.state 
+        let string = e.target.value
+        this.setState({
+            type :e.target.value 
+        },
+        )
+         filteredItems = filteredItems.filter(item => item.type.includes(string))
+        this.setState({filteredItems:filteredItems})
+    }
     render() {
-
         return (
             <div>
             <br />
             <div className = "container text-center border border-light p-9">
                 <h2>Clothing</h2>
-                <input name="search" className="form-control" onChange={e => this.onSearch(e)} value={this.state.SearchString}  placeholder="Search for item Name"/>
+                <lable>
+                    Select by category..
+                    <select
+                     ref = "userInput"
+                     required ="true"
+                     value = {this.state.category}
+                     onChange = {this.onChangecategory.bind(this)}
+                     >
+                         <option value = ""></option>
+                         <option value = "Women">Women</option>
+                         <option value = "Men">Men</option>
+                         <option value = "Kids">Kids</option>
+                    </select>
+                </lable>
+                <lable>
+                    Select by type..
+                    <select
+                     ref = "userInput"
+                     required ="true"
+                     value = {this.state.type}
+                     onChange = {this.onChangetype.bind(this)}
+                     >
+                         <option value = ""></option>
+                         <option value = "Shoes">Shoes</option>
+                         <option value = "Dress">Dress</option>
+                         <option value = "Jacket">Jacket</option>
+                         <option value = "Blouse">Blouse</option>
+                         <option value = "Gloves">Gloves</option>
+                         <option value = "Hat">Hat</option>
+                         <option value = "Scarf">Scarf</option>
+                    </select>
+                </lable>
                 <table className = "table">
                 <thead className = "thead">
                     <tr>
@@ -95,7 +131,6 @@ class ItemsList extends Component {
                         <th>Description</th>
                         <th>Donor Phone Number</th>
                         <th>Image</th>
-                        
                     </tr>
                 </thead>
                 <tbody>
@@ -108,5 +143,4 @@ class ItemsList extends Component {
         )
     }
 }
-
 export default withRouter(ItemsList)
